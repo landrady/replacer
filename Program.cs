ca14ef0055978ac30060bd07f617dfe1e1adbd75
Leonardo de Andrade <leonardo.andrade@hiplatform.com>
2018-06-29 09:05:52 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace replacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1){
+                Console.WriteLine("Passe o caminho como parametro");
+            }else{
+                ReadFiles(args[0]);
+            }
+        }
+
+        static void ReadFiles(string path){
+            Console.WriteLine(path);
+            Console.WriteLine("Carregando quantidade de arquivos na pasta.");
+            var files = Directory.EnumerateFiles(path,"*",SearchOption.TopDirectoryOnly);
+            Console.WriteLine(files.Count());
+            //foreach(var file in files){
+                //Console.WriteLine(file);
+
+            //}
+        }
+    }
+}
